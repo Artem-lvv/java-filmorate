be0d6b1b3e5458fb4ba41a1541aec2e2dc9c08d6
Artem Shidlov <Artem.lvv@yandex.ru>
2024-06-11 00:50:24 +0300

    delete line break (test git)

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -340,7 +340,6 @@ public class FilmService implements FilmStorage {
             if (recommendedFilms.size() >= maxSize) {
                 break;
             }
-
         }
 
         return recommendedFilms
